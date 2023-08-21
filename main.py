fd45717bbd939d7cb08694f3a5b916c4941beba6
Ivan Fedorov <idfedorov09@gmail.com>
2023-08-21 18:21:28 +0300

    async added

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import config
+import asyncio
 
 
-def personal_message_handler(event):
+async def personal_message_handler(event):
     pass
 
 
-def chat_message_handler(event):
+async def chat_message_handler(event):
     pass
 
 
-if __name__ == "__main__":
+async def main():
     token = config.token
     group_id = config.group_id
 
     vk_session = vk_api.VkApi(token=token)
     receiver = vk_api.bot_longpoll.VkBotLongPoll(vk_session, group_id)
-    vk = vk_session.get_api()
 
     for event in receiver.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             if event.from_user:
-                personal_message_handler(event)
+                await personal_message_handler(event)
             else:
-                chat_message_handler(event)
+                await chat_message_handler(event)
+
+if __name__ == "__main__":
+    asyncio.run(main())
