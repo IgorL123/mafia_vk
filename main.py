04c695c7d090c8deec721e167e210a37866e72ae
Ivan Fedorov <idfedorov09@gmail.com>
2023-08-21 17:00:16 +0300

    rewritten

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import config
 
 
-async def personal_message_handler(event):
+def personal_message_handler(event):
     pass
 
 
-async def chat_message_handler(event):
+def chat_message_handler(event):
     pass
 
 
 if __name__ == "__main__":
     token = config.token
-    vk = vk_api.VkApi(token=token)
-    receiver = VkLongPoll(vk)
+    group_id = config.group_id
+
+    vk_session = vk_api.VkApi(token=token)
+    receiver = vk_api.bot_longpoll.VkBotLongPoll(vk_session, group_id)
+    vk = vk_session.get_api()
 
     for event in receiver.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.to_me:
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            if event.from_user:
                 personal_message_handler(event)
             else:
                 chat_message_handler(event)
